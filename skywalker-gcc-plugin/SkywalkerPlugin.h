e3f95542dc7a01892ede4001f7589cdfcbcd6c20
Shyfan <410713847@qq.com>
2024-08-12 15:45:19 +0800

    增加插件能力

--- skywalker-gcc-plugin/SkywalkerPlugin.h
+++ skywalker-gcc-plugin/SkywalkerPlugin.h
@@ -10,6 +10,8 @@
 
 #include <cstdarg>
 #include <cstdio>
+#include <map>
+#include <typeinfo>
 
 #include <gcc-plugin.h>
 #include <plugin-version.h>
@@ -18,6 +20,8 @@
 #include <diagnostic.h>
 #include <vec.h>
 #include <function.h>
+
+#include "IPluginAbility.h"
 
 /**
  * 插件信息
@@ -55,25 +59,122 @@ public:
 
     /**
      * Log Debug
-    */
+     */
     void Debug(const char *format, ...);
 
     /**
      * Log Info
-    */
+     */
     void Info(const char *format, ...);
 
     /**
      * Log Warning
-    */
+     */
     void Warning(const char *format, ...);
 
     /**
      * Log Error
-    */
+     */
     void Error(const char *format, ...);
 
 #pragma endregion Log
+
+#pragma region Plugin Ability
+
+    /**
+     * 注册插件能力
+     */
+    template <typename T>
+    void RegisterPluginAbility()
+    {
+        // 获取 T 类型的名称
+        const char *abilityName = typeid(T).name();
+        if (abilityName == nullptr)
+        {
+            Error("RegisterPluginAbility failed, abilityName is nullptr");
+            return;
+        }
+
+        // 判断是否已经注册
+        if (PluginAbilityMap.find(abilityName) != PluginAbilityMap.end())
+        {
+            Error("RegisterPluginAbility failed, abilityName: %s has been registered", abilityName);
+            return;
+        }
+
+        // 创建插件能力
+        IPluginAbility *pluginAbility = new T();
+        if (pluginAbility == nullptr)
+        {
+            Error("RegisterPluginAbility failed, create abilityName: %s failed", abilityName);
+            return;
+        }
+
+        // 注册插件能力
+        pluginAbility->Register();
+
+        // 添加到插件能力列表
+        PluginAbilityMap[abilityName] = pluginAbility;
+    }
+
+    /**
+     * 反注册插件能力
+     */
+    template <typename T>
+    void UnRegisterPluginAbility()
+    {
+        // 获取 T 类型的名称
+        const char *abilityName = typeid(T).name();
+        if (abilityName == nullptr)
+        {
+            Error("UnRegisterPluginAbility failed, abilityName is nullptr");
+            return;
+        }
+
+        // 查找插件能力
+        auto iter = PluginAbilityMap.find(abilityName);
+        if (iter == PluginAbilityMap.end())
+        {
+            Error("UnRegisterPluginAbility failed, abilityName: %s not found", abilityName);
+            return;
+        }
+
+        // 反注册插件能力
+        iter->second->UnRegister();
+
+        // 释放插件能力
+        iter->second->Release();
+
+        // 从插件能力列表中移除
+        PluginAbilityMap.erase(iter);
+    }
+
+    /**
+     * 获取插件能力
+     */
+    template <typename T>
+    T *GetPluginAbility()
+    {
+        // 获取 T 类型的名称
+        const char *abilityName = typeid(T).name();
+        if (abilityName == nullptr)
+        {
+            Error("GetPluginAbility failed, abilityName is nullptr");
+            return nullptr;
+        }
+
+        // 查找插件能力
+        auto iter = PluginAbilityMap.find(abilityName);
+        if (iter == PluginAbilityMap.end())
+        {
+            Error("GetPluginAbility failed, abilityName: %s not found", abilityName);
+            return nullptr;
+        }
+
+        return dynamic_cast<T *>(iter->second);
+    }
+
+#pragma endregion Plugin Ability
 
     /**
      * 获取插件帮助
@@ -82,6 +183,9 @@ public:
 
 public:
     static SSkywalkerPluginInfo PluginInfo;
+
+private:
+    std::map<const char *, IPluginAbility *> PluginAbilityMap;
 };
 
 extern "C" SkywalkerPlugin skywalker_plugin;
---
+++ skywalker-gcc-plugin/plugin-ability/IPluginAbility.h
@@ -0,0 +1,30 @@
+﻿/*************************************************************************
+**文件: skywalker-gcc-plugin/plugin-ability/IPluginAbility.h
+**作者: shyfan
+**日期: 2024/07/30 17:30:05
+**功能: 插件能力接口
+*************************************************************************/
+
+#ifndef __I_PLUGIN_ABILITY_H__
+#define __I_PLUGIN_ABILITY_H__
+
+class IPluginAbility
+{
+public:
+    /**
+     * 注册
+     */
+    virtual void Register() = 0;
+
+    /**
+     * 反注册
+     */
+    virtual void UnRegister() = 0;
+
+    /**
+     * 释放
+     */
+    virtual void Release() = 0;
+};
+
+#endif // __I_PLUGIN_ABILITY_H__
